edc274ebcb96fd03154c03adec744c16f4d939a4
lux00leo <lux00leo@gmail.com>
2024-03-25 17:55:40 +0900

    footer v2

--- app/ui/hkdemo/footer/footer.tsx
+++ app/ui/hkdemo/footer/footer.tsx
@@ -3,18 +3,18 @@ import {locationData} from "@/app/lib/hk/data";
 
 export default function Footer() {
     return (
-        <div className={'w-full flex h-[60vh] sm:h-[25vh] bg-neutral-800 items-center sm:px-[5%] sm:text-sm'}>
-            <div className={'sm:grow sm:basis-0 flex sm:flex-col text-neutral-300 sm:items-start'}>
+        <div className={'w-full flex flex-col sm:flex-row h-[30vh] sm:h-[25vh] bg-neutral-800 items-center sm:px-[5%] sm:text-sm mt-10 sm:mt-20'}>
+            <div className={'grow order-2 sm:order-1 sm:basis-0 flex text-xs sm:text-sm sm:flex-col text-neutral-300 sm:items-start'}>
                 Addr) {`${locationData[0].streetAddress} ${locationData[0].buildingInfo}`}
             </div>
-            <div className={'sm:grow sm:basis-0 flex sm:flex-col items-center'}>
-                <div className={'md:w-[15vw] w-[18vw] flex items-center'}>
+            <div className={'grow order-1 sm:order-2 sm:basis-0 flex sm:flex-col items-center'}>
+                <div className={'md:w-[15vw] w-[50vw] flex items-center'}>
                         <Image className={'w-full'} src={"/hkdemo/hkenc_logo.png"} alt={"company logo"} width={1000}
                                height={300}
                                priority={true}/>
                 </div>
             </div>
-            <div className={'sm:grow sm:basis-0 flex sm:flex-col text-neutral-300 sm:items-end'}>
+            <div className={'grow order-3 sm:basis-0 flex sm:flex-col text-xs sm:text-md text-neutral-300 sm:items-end'}>
                 copyright ⓒ HK E&C. All right Reserved.
             </div>
         </div>
