07e1c5e2b623c4289c4035e973cec91e582af127
lux00leo <lux00leo@gmail.com>
2024-04-02 13:08:07 +0900

    change title icon

--- app/ui/hkdemo/overview/section_title.tsx
+++ app/ui/hkdemo/overview/section_title.tsx
@@ -18,8 +18,11 @@ export default function SectionTitle({title}:{
 
 function TitleIcon() {
     return (
-        <div className={'flex items-center justify-center bg-[#ff1919] w-5 h-5 sm:w-6 sm:h-6 rounded-br-lg rounded-tl-lg'}>
-            <div className={'bg-white w-3 h-3 sm:w-4 sm:h-4 rounded-br-sm rounded-tl-sm'}></div>
+        <div className={'flex relative'}>
+            <div
+                className={'flex items-center justify-center border-4 border-red-600 w-5 h-5 sm:w-6 sm:h-6 rounded-br-lg rounded-tl-lg'}>
+                <div className={'w-3 h-3 sm:w-4 sm:h-4 rounded-br-sm rounded-tl-sm'}></div>
+            </div>
         </div>
-    )
+    );
 }
