d87a2aba1ee874606fa29a5733e721b797d1e21c
lux00leo <lux00leo@gmail.com>
2024-07-02 10:52:57 +0900

    24.07.02 fix

--- app/subpage/rnd/licences/page.tsx
+++ app/subpage/rnd/licences/page.tsx
@@ -1,10 +1,10 @@
 import {Suspense} from "react";
 import SectionTitle from "@/app/ui/hkdemo/overview/section_title";
 import LicenceList from "@/app/ui/subpage/rnd/licence/licence_list";
-import {getLicences} from "@/app/subpage/rnd/action";
+// import {getLicences} from "@/app/subpage/rnd/action";
+import {licences} from "@/app/ui/subpage/rnd/licence/licence_dummy_data";
 
 export default async function Page(){
-    const licences = await getLicences();
     return (
         <div className={'w-full pb-10 sm:pb-20 px-[5%] sm:px-[10%]'}>
             <Suspense>
